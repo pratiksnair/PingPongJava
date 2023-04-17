a7c2c22c9e2dfb4b35e64aee49ba96ee7eaa9c48
pratiksnair <75872869+pratiksnair@users.noreply.github.com>
2023-04-17 23:23:37 +0530

    Add files via upload

---
+++ src/GameFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    GamePanel panel;
+
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ src/PongGame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+public class PongGame {
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+
+    }
+}
